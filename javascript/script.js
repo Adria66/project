49719d4d36abec46c6964667bea97a0885909f4d
Adria <ironhackadria@DESKTOP-Q36778H.localdomain>
2020-11-02 18:26:39 +0100

    3 commit

--- javascript/script.js
+++ javascript/script.js
@@ -21,26 +21,14 @@ pop.src = "/images/pop.png"
 // DRAW BALLOONS
 const weight = 75;
 const height = 100;
+
+let mouseX = 0;
+let tmouseY = 0;
+
 const balloons = [redBalloon, blueBalloon, yellowBalloon, greenBalloon];
 
-const drawBalloon = () => {
-    ctx.drawImage (randomBalloons0, 75, 175, weight, height);
-    ctx.drawImage (randomBalloons1, 200, 175, weight, height);
-    ctx.drawImage (randomBalloons2, 325, 175, weight, height);
-    ctx.drawImage (randomBalloons3, 450, 175, weight, height);
-    ctx.drawImage (randomBalloons4, 75, 25, weight, height);
-    ctx.drawImage (randomBalloons5, 200, 25, weight, height);
-    ctx.drawImage (randomBalloons6, 325, 25, weight, height);
-    ctx.drawImage (randomBalloons7, 450, 25, weight, height);
-};
-
-// POSITION (NO SE COMO HACERLO)
-
 let balloonX = [75, 200, 325, 450];
-let balloonY = [25, 175]; 
- 
-
-// RANDOM BALLOONS (FOR EACH/ FOR OF)
+let balloonY = [25, 175];
 
 const randomBalloons0 = balloons[Math.floor(Math.random() * balloons.length)];
 const randomBalloons1 = balloons[Math.floor(Math.random() * balloons.length)];
@@ -51,74 +39,161 @@ const randomBalloons5 = balloons[Math.floor(Math.random() * balloons.length)];
 const randomBalloons6 = balloons[Math.floor(Math.random() * balloons.length)];
 const randomBalloons7 = balloons[Math.floor(Math.random() * balloons.length)];
 
+let randomBallons = [
+  {
+    name: randomBalloons0,
+    x: 75,
+    y: 175,
+    width: weight,
+    height: height,
+  },
+  {
+    name: randomBalloons1,
+    x: 200,
+    y: 175,
+    width: weight,
+    height: height,
+  },
+  {
+    name: randomBalloons2,
+    x: 325,
+    y: 175,
+    width: weight,
+    height: height,
+  },
+  {
+    name: randomBalloons3,
+    x: 450,
+    y: 175,
+    width: weight,
+    height: height,
+  },
+  {
+    name: randomBalloons4,
+    x: 75,
+    y: 25,
+    width: weight,
+    height: height,
+  },
+  {
+    name: randomBalloons5,
+    x: 200,
+    y: 25,
+    width: weight,
+    height: height,
+  },
+  {
+    name: randomBalloons6,
+    x: 325,
+    y: 25,
+    width: weight,
+    height: height,
+  },
+  {
+    name: randomBalloons7,
+    x: 450,
+    y: 25,
+    width: weight,
+    height: height,
+  },
+];
+
+const drawBalloon = () => {
+  randomBallons.forEach((balloon) => {
+    return ctx.drawImage(
+      balloon.name,
+      balloon.x,
+      balloon.y,
+      balloon.width,
+      balloon.height
+    );
+  });
+};
 
 
+//------------ Función para sustituir los globos -----------------
+
+const checkBallons = (mouseX, mouseY) => {
+  randomBallons = randomBallons.map((balloon) => {
+    if (
+      mouseX > balloon.x &&
+      mouseX < balloon.x + balloon.width &&
+      mouseY > balloon.y &&
+      mouseY < balloon.y + balloon.height
+    ) {
+      return {
+        name: pop,
+        x: balloon.x,
+        y: balloon.y,
+        width: balloon.width,
+        height: balloon.height,
+        
+      };
+    } else {
+      return balloon;
+    }
+  });
+  
+  return randomBallons;
+};
+
+let popTime = setInterval(() => {
+    pop1(checkBallons)
+}, 2000);
+function pop1(){
+    ctx.beginPath();
+    ctx.fillStyle = "#ffffff";
+    ctx.rect(this.x,this.y,75,100)
+    ctx.fill();
+}
 // LOADED IMAGES
 
 counter = 0;
 
 blueBalloon.onload = () => {
-	counter++;
-	checkIfAllImagesAreLoaded();
+  counter++;
+  checkIfAllImagesAreLoaded();
 };
 redBalloon.onload = () => {
-	counter++;
-	checkIfAllImagesAreLoaded();
+  counter++;
+  checkIfAllImagesAreLoaded();
 };
 yellowBalloon.onload = () => {
-	counter++;
-	checkIfAllImagesAreLoaded();
+  counter++;
+  checkIfAllImagesAreLoaded();
 };
 greenBalloon.onload = () => {
-	counter++;
-	checkIfAllImagesAreLoaded();
+  counter++;
+  checkIfAllImagesAreLoaded();
 };
 
 const checkIfAllImagesAreLoaded = () => {
-	if (counter === 4) {
-		startGame();
-	}
+  if (counter === 4) {
+    startGame();
+  }
 };
 
 // FUNCTION START
 
 const startGame = () => {
-    clearCanvas()
-    drawBalloon()
+  drawBalloon();
+};
+
+const position = [];
+const clearCanvas = () => {
+  ctx.clearRect(0, 0, 600, 300);
+};
+// MOUSECLICK POSITION
+function getMousePosition(canvas, event) {
+  let rect = canvas.getBoundingClientRect();
+  mouseX = event.clientX - rect.left;
+  mouseY = event.clientY - rect.top;
+  console.log("Coordinate x: " + mouseX, "Coordinate y: " + mouseY);
 }
 
-
-const clearCanvas = () => {
-	ctx.clearRect(0, 0, 600, 300);
-};
- 
-
-// MOUSECLICK POSITION + DRAW POP
-
-function getMousePosition(canvas, event) { 
-    let rect = canvas.getBoundingClientRect(); 
-    let x = event.clientX - rect.left; 
-    let y = event.clientY - rect.top; 
-    
-    ctx.drawImage (pop , x-35, y-50, weight, height)
-    console.log("Coordinate x: " + x,  
-                "Coordinate y: " + y); 
-} 
-
-let canvasElem = document.querySelector("canvas"); 
-  
-canvasElem.addEventListener("mousedown", function(e) 
-{ 
-    getMousePosition(canvasElem, e);  
+let canvasElem = document.querySelector("canvas");
+canvasElem.addEventListener("mousedown", function (e) {
+  getMousePosition(canvasElem, e);
+  checkBallons(mouseX, mouseY);
+  drawBalloon();
 });
-
-
-// for (i = 0; i < balloons.length; i++) {
-//     let ball = balloons[i];
-//     const drawBalloon1 = () => {
-//         const randomBalloons = ball[Math.floor(Math.random() * balloons.length)];
-//         ctx.drawImage (randomBalloons, 300, 150, weight, height);
-//         clearCanvas()
-//         drawBalloon1()
-//     };   
-// };
